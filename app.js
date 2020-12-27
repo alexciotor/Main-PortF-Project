1a1518608caf8bd4e13f71ee957617b6ef735a69
alexciotor <ciotor.alex@yahoo.com>
2020-12-27 17:08:18 +0200

    Added images and Portfolio section

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const buttons = document.querySelectorAll('.menu-lists');
 const menu = document.querySelector('.main-menu')
-
+const aboutMe = document.querySelector('.about-me')
+const right = document.querySelector('.right-side-content')
 menu.addEventListener('click',(e)=>{
     const id = e.target.dataset.id
      
     if(id){
 buttons.forEach((button)=>{
- 
     button.classList.remove('active')
     e.target.classList.add('active')
  
- 
- 
-     
-
 })
     }
 })
 
- 
+
+
+ const btn = document.querySelector('.btn')
+ btn.addEventListener('click',()=>{
+     right.classList.remove('right-side-content')
+     right.classList.add('move')
+    
+
+ })
