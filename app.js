3281019c587000a5f771f3c2b9bd5bc6535794a4
alexciotor <ciotor.alex@yahoo.com>
2021-01-04 06:33:53 +0200

    must add resize

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const buttons = document.querySelectorAll('.menu-lists');
 const menu = document.querySelector('.main-menu')
 const right = document.querySelector('.right-side-content')
 const main = document.querySelector('.main-content')
-const rect = main.getBoundingClientRect()
-const abc = rect.width
-console.log(abc);
+
 const home = document.querySelector('.home-main-section')
 const portfolio = document.querySelector('.portfolio')
 const certificates = document.querySelector('.certificates')
@@ -12,23 +10,21 @@ const aboutMe = document.querySelector('.about-me')
 const contact = document.querySelector('.contact')
  const menuBtn = document.querySelector('.menu-btn')
 const menuBar = document.querySelector('.left-margin')
-
- menuBtn.addEventListener('click', (e)=>{
-     if(menuBar.classList.contains('menu-display')){
-         menuBar.classList.remove('menu-display')
-     }
-     else{
-         menuBar.classList.add('menu-display')
-       
-        
-         
-     }
- })
-
- const change = (id)=>{
+const sections = document.querySelector('.sections')
+console.log(sections);
+const rect = main.getBoundingClientRect().width
+let abc = 0
+window.addEventListener('resize',()=>{
+    const data= main.getBoundingClientRect().width
+    abc = data
+    console.log(abc);
+})
+console.log(abc);
+const change = (id)=>{
+   
+      
      const main = document.querySelector('.main-content')
-const rect = main.getBoundingClientRect()
- 
+
      const menuBar = document.querySelector('.left-margin')
      if(id==0){
 portfolio.classList.remove('add')
@@ -37,8 +33,8 @@ aboutMe.classList.remove('add')
 home.classList.remove('none')
 contact.classList.remove('add')
 home.classList.add('disp')
- if(abc < 761){
-     menuBar.classList.remove('menu-display')
+ if(rect < 761){
+ menuBar.classList.remove('menu-display')  
  }
      }
      if(id==1){
@@ -48,10 +44,9 @@ home.classList.add('disp')
          home.classList.add('none')
          contact.classList.remove('add')
          aboutMe.classList.add('add')
-         if(abc < 761){
-     menuBar.classList.remove('menu-display')
+         if(rect < 761){
+     menuBar.classList.remove('menu-display')  
  }
-
      }
     if(id==2){
        home.classList.remove('disp')
@@ -60,11 +55,9 @@ home.classList.add('disp')
        certificates.classList.remove('add')
        contact.classList.remove('add')
        portfolio.classList.add('add')
-       if(abc < 761){
-        menuBar.classList.remove('menu-display')
-
+       if(rect < 761){
+     menuBar.classList.remove('menu-display')  
  }
-       
     }
     if(id==3){
         home.classList.remove('disp')
@@ -74,12 +67,9 @@ home.classList.add('disp')
         contact.classList.remove('add')
         certificates.classList.add('add')
         console.log(certificates);
-     if(abc < 761){
-     menuBar.classList.remove('menu-display')
-     
+     if(rect < 761){
+     menuBar.classList.remove('menu-display')   
  }
-   
-  
     }
     if(id==4){
            home.classList.remove('disp')
@@ -88,31 +78,25 @@ home.classList.add('disp')
        portfolio.classList.remove('add')
        certificates.classList.remove('add')
        contact.classList.add('add')
-       if(abc < 761){
-     menuBar.classList.remove('menu-display')
-     
+       if(rect < 761){
+     menuBar.classList.remove('menu-display')   
  }
-       
-    
-    
-        
     }
  }
 
+ menuBtn.addEventListener('click', (e)=>{
+    menuBar.classList.toggle('menu-display')
+ })
+ 
 menu.addEventListener('click',(e)=>{
-    const id = e.target.dataset.id
-     
+    const id = e.target.dataset.id   
     if(id){
 buttons.forEach((button)=>{
     button.classList.remove('active')
     e.target.classList.add('active')
-    
- 
 })
-
     }
     change(id)
- 
 })
 
 
