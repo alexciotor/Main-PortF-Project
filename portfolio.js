414ef78f510acb9f134c705281ed6b819adc24f9
alexciotor <ciotor.alex@yahoo.com>
2021-02-16 14:27:31 +0200

    drink project removed

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,10 @@
 export const Portfolio = [
-    {
-        name: 'CocktailDB API',
-        type: 'React (in progress..)',
-        img: './photos/projects/db.png',
-        href: 'https://relaxed-beaver-dea84f.netlify.app/'
-    },
+    // {
+    //     name: 'CocktailDB API',
+    //     type: 'React (in progress..)',
+    //     img: './photos/projects/db.png',
+    //     href: 'https://relaxed-beaver-dea84f.netlify.app/'
+    // },
     {
         name: 'Grocery List',
         type: 'React Project',
